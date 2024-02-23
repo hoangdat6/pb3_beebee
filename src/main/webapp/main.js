2d8733019e64177a9b7037106cb946b4b19889be
vulinhnopro2704 <vulinhtruong79@gmail.com>
2024-02-23 20:35:07 +0700

    Add Small Cart and Cart

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,14 +1,15 @@
 "use strict";
-function increaseQuantity() {
-    let input = document.querySelector('.Qty__Input');
+
+function increaseQuantity(button) {
+    let input = button.parentNode.querySelector('.Qty__Input');
     let value = parseInt(input.value);
     input.value = value + 1;
-  }
-  
-  function decreaseQuantity() {
-    let input = document.querySelector('.Qty__Input');
+}
+
+function decreaseQuantity(button) {
+    let input = button.parentNode.querySelector('.Qty__Input');
     let value = parseInt(input.value);
     if (value > 1) {
-      input.value = value - 1;
+        input.value = value - 1;
     }
-  }
+}
